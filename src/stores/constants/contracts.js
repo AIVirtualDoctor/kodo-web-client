4cc766dcaa2f03716db05aefd0d2a6459326a2bb
you <you@example.com>
2025-07-25 15:53:52 +0530

    Update contract addresses and permissions; modify WETH and ETH token details in constants

--- src/stores/constants/contracts.js
+++ src/stores/constants/contracts.js
@@ -14,24 +14,25 @@ export const VE_TOKEN_DECIMALS = 18
 export const VE_TOKEN_LOGO = '/tokens/kodo.svg'
 export const VE_TOKEN_ABI = abis.veTokenABI
 
-export const WETH_ADDRESS = '0xc42baa20e3a159cf7a8adfa924648c2a2d59e062'
-export const WETH_NAME = 'Wrapped Ether'
-export const WETH_SYMBOL = 'WETH'
+export const WETH_ADDRESS = '0x1aE9c40eCd2DD6ad5858E5430A556d7aff28A44b'
+export const WETH_NAME = 'Wrapped Camp'
+export const WETH_SYMBOL = 'WCAMP'
 export const WETH_DECIMALS = 18
 export const WETH_ABI = abis.wethABI
 
+export const ETH_NAME = 'CAMP'
 export const ETH_ADDRESS = 'ETH'
-export const ETH_NAME = 'Ether'
-export const ETH_SYMBOL = 'ETH'
+export const ETH_SYMBOL = 'CAMP'
 export const ETH_DECIMALS = 18
-export const ETH_LOGO = 'https://static.optimism.io/data/ETH/logo.svg'
+export const ETH_LOGO =
+  'https://raw.githubusercontent.com/summitx-finance/tokens/main/images/123420001114/0x1ae9c40ecd2dd6ad5858e5430a556d7aff28a44b.png'
 
 export const FACTORY_ADDRESS = '0x1340650d3AbFdd81a7ec3d4C9b48E46c3C8E90a8' // PairFactory
 export const FACTORY_ABI = abis.factoryABI
 export const STABLE_FEE_BPS = 2 // 0.02%
 export const VOLATILE_FEE_BPS = 20 // 0.2%
 
-export const ROUTER_ADDRESS = '0x79656773c241CB9dd6939E54e7c44d06431Dba51'
+export const ROUTER_ADDRESS = '0xdE915e026091a617BF6C41f43C5214d058057aa7'
 export const ROUTER_ABI = abis.routerABI
 
 export const VE_DIST_ADDRESS = '0x8302E88a0bB88e8522e0A0F8C9fCb0e0036D0AF8'
@@ -41,7 +42,7 @@ export const VE_DIST_EPOCH_0_START = 1718236800
 export const VOTER_ADDRESS = '0x4aD9c058B73ceFffc22A5d70FFfD5d2DBC05De4A'
 export const VOTER_ABI = abis.voterABI
 
-export const LIBRARY_ADDRESS = '0x62D081F7Fb6D1B6e248E9E04D5acF89bf1530455'
+export const LIBRARY_ADDRESS = '0x2751004CaF0b0fE7a743694bAb94402A107C1367'
 export const LIBRARY_ABI = abis.kodoLibraryABI
 
 // export const MERKLE_CLAIM_ADDRESS = '0x7E034Ef620D2fb403e8bB6a1130670110287A7a1'
@@ -56,4 +57,4 @@ export const FEES_ABI = abis.feesABI
 export const BRIBE_ABI = abis.bribeABI
 export const TOKEN_ABI = abis.tokenABI
 
-export const MULTICALL_ADDRESS = '0xcb2436774C3e191c85056d248EF4260ce5f27A9D'
+export const MULTICALL_ADDRESS = '0xe70E6Acc51fc76ceCd98ae97bD3052e3aD02Db31'
